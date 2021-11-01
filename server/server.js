0ea3eb3a539121d0abaa56f21a86d1716efca126
unknown <richdanmac@gmail.com>
2021-11-01 13:36:36 -0500

    init commit

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs')
+let app = express();
+let router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.post('/formsubmissions', (req, res, next) => {
+    console.log(req.body.name)
+    console.log(req.body.email)
+    fs.writeFile('form-submissions.json', JSON.stringify(req.body), err => { if (err) throw err });
+    res.redirect('/form-submissions');
+    next();
+})
+
+app.get("/form-submissions", (req, res, next) => {
+    fs.readFile('form-submissions.json', (err) => {
+        if (err) throw err;
+    });
+    next();
+})
+
+app.use('/', (req, res, next) => {
+    console.log(req.url)
+    next();
+})
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.use(router)
+
+app.listen(3000)
